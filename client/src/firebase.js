9b3df262f6aaf86eed95153534c8064682bca0d5
Akshay Poojary <akshaypoojary167@gmail.com>
2026-01-21 10:01:32 +0530

    security: remove hardcoded Firebase keys, use env vars only

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,12 +7,12 @@ import { config } from "./config";
 
 // Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: config.firebase.apiKey || "AIzaSyBXg4i1QeVLcnDyEfdw9-46ts920KtU9Lk",
-  authDomain: config.firebase.authDomain || "office-sync-4eae8.firebaseapp.com",
-  projectId: config.firebase.projectId || "office-sync-4eae8",
-  storageBucket: config.firebase.storageBucket || "office-sync-4eae8.firebasestorage.app",
-  messagingSenderId: config.firebase.messagingSenderId || "403466702178",
-  appId: config.firebase.appId || "1:403466702178:web:63c549bf4ab8cd1a8d703b",
+  apiKey: config.firebase.apiKey,
+  authDomain: config.firebase.authDomain,
+  projectId: config.firebase.projectId,
+  storageBucket: config.firebase.storageBucket,
+  messagingSenderId: config.firebase.messagingSenderId,
+  appId: config.firebase.appId,
 };
 
 // Initialize Firebase
