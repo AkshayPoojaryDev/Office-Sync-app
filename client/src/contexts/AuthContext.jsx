dad8bfef8dcba0e2cb725955ebab9b379e5d87b1
Akshay Poojary <akshaypoojary167@gmail.com>
2026-01-28 14:43:21 +0530

    feat: implement AuthContext for managing user authentication state and roles

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -9,7 +9,7 @@ const AuthContext = createContext();
 export function useAuth() {
   return useContext(AuthContext);
 }
-
+// AuthProvider component
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
   const [userRole, setUserRole] = useState(null);
